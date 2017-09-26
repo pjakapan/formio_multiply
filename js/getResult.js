e20de6a8a467f99f73899e0786f8569291502bc8
pjakapan <j.petchan@togetherteam.co.th>
2017-09-26 14:47:16 +0700

    adf

--- js/getResult.js
+++ js/getResult.js
@@ -1,36 +1,27 @@
+
 // -----------------------------------------------------------------------------------------------------------------------------
 // Load data form azure : start (API)
 // -----------------------------------------------------------------------------------------------------------------------------
 function getMuls() {
+
     var getsettings = {
         "async": true,
         "crossDomain": true,
         "url": "https://togetherdemo.azure-api.net/calc/mul",
         "method": "GET"
     }
-
+    var res_html = "";
     $.ajax(getsettings).done(function (response) {
-
-        var res_html = "";
-        var res_id = "";
-        var res_inputA = "";
-        var res_inputB = "";
-        var res_result = "";
-        var res_datetimeStamp = "";
-
-        // -----------------------------------------------
-        // bind Results to inner.html
-        // -----------------------------------------------
-
         $.each(response, function (key, value) {
-            res_id = JSON.stringify(value.ID);
-            res_inputA = value.InputA;
-            res_inputB = value.InputB;
-            res_result = value.Result;
-            res_datetimeStamp = value.Datetimestamp;
+            var res_id = JSON.stringify(value.ID);
+            var res_inputA = value.InputA;
+            var res_inputB = value.InputB;
+            var res_result = value.Result;
+            var res_datetimeStamp = value.Datetimestamp;
             res_html += "<p class='info'>" + "<strong>ID: " + res_id + "</strong> " + "(" + res_inputA + " x " + res_inputB + ") = " + res_result + "<span class='pull-right' style='color:blue'>" + res_datetimeStamp + "</span>" + "</p>";
         });
-        return res_html;
-
     });
+
+    return res_html;
+
 }
