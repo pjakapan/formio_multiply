2567143b51a9d0cbdac0cf1f4b98e8cd0f1fafbc
pjakapan <j.petchan@togetherteam.co.th>
2017-09-26 15:00:41 +0700

    asdfsadf

--- js/getResult.js
+++ js/getResult.js
@@ -11,17 +11,18 @@ function getMuls(mul) {
         "url": "https://togetherdemo.azure-api.net/calc/" + mul,
         "method": "GET"
     }
-    $.ajax(getsettings).done(function (response) {
+    res_html = $.ajax(getsettings).done(function (response) {
+        var html = "";
         $.each(response, function (key, value) {
             var res_id = JSON.stringify(value.ID);
             var res_inputA = value.InputA;
             var res_inputB = value.InputB;
             var res_result = value.Result;
             var res_datetimeStamp = value.Datetimestamp;
-            res_html += "<p class='info'>" + "<strong>ID: " + res_id + "</strong> " + "(" + res_inputA + " x " + res_inputB + ") = " + res_result + "<span class='pull-right' style='color:blue'>" + res_datetimeStamp + "</span>" + "</p>";
+            html += "<p class='info'>" + "<strong>ID: " + res_id + "</strong> " + "(" + res_inputA + " x " + res_inputB + ") = " + res_result + "<span class='pull-right' style='color:blue'>" + res_datetimeStamp + "</span>" + "</p>";
         });
+        return html;
     });
-    console.log(res_html);
-    return res_html;
 
+    return res_html;
 }
