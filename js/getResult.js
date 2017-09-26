464fcb4564646fe544d86cda7f1d78d5759b89da
pjakapan <j.petchan@togetherteam.co.th>
2017-09-26 15:13:01 +0700

    asdf

--- js/getResult.js
+++ js/getResult.js
@@ -3,8 +3,24 @@
 // Load data form azure : start (API)
 // -----------------------------------------------------------------------------------------------------------------------------
 
+
 function getMuls(mul) {
     var res_html = "";
+    var response = callAPI(mul);
+    jQuery.each(response, function (key, value) {
+        var res_id = JSON.stringify(value.ID);
+        var res_inputA = value.InputA;
+        var res_inputB = value.InputB;
+        var res_result = value.Result;
+        var res_datetimeStamp = value.Datetimestamp;
+        res_html += "<p class='info'>" + "<strong>ID: " + res_id + "</strong> " + "(" + res_inputA + " x " + res_inputB + ") = " + res_result + "<span class='pull-right' style='color:blue'>" + res_datetimeStamp + "</span>" + "</p>";
+    });
+    return res_html;
+}
+
+
+function callAPI(mul) {
+    var response = "";
     var getsettings = {
         "async": true,
         "crossDomain": true,
@@ -12,17 +28,8 @@ function getMuls(mul) {
         "method": "GET"
     }
     $.ajax(getsettings).done(function (response) {
-        $.each(response, function (key, value) {
-            var res_id = JSON.stringify(value.ID);
-            var res_inputA = value.InputA;
-            var res_inputB = value.InputB;
-            var res_result = value.Result;
-            var res_datetimeStamp = value.Datetimestamp;
-            res_html += "<p class='info'>" + "<strong>ID: " + res_id + "</strong> " + "(" + res_inputA + " x " + res_inputB + ") = " + res_result + "<span class='pull-right' style='color:blue'>" + res_datetimeStamp + "</span>" + "</p>";
-        });
-        console.log(res_html);
+        response = response;
     });
-
-    console.log(res_html);
-    return res_html;
+    return response;
 }
+
